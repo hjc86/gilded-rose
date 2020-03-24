eaed8380c398e6037b15a3614cb471817cfffc08
Helder Carmen <heldercarmen@gmail.com>
2020-03-24 11:49:26 +0000

    delete comments

--- js-mocha/src/gilded_rose.js
+++ js-mocha/src/gilded_rose.js
@@ -15,14 +15,6 @@ class Shop {
 
     for (var i = 0; i < this.items.length; i++) {
 
-      //for loop begins
-      // if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-      //   if (this.items[i].quality > 0) {
-      //     if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-      //       this.items[i].quality = this.items[i].quality - 1; // reduce quality by 1 unit
-      //     }
-      //   }
-      // }
       
       if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert' 
           && (this.items[i].quality > 0) && this.items[i].name != 'Sulfuras, Hand of Ragnaros') { 
@@ -31,24 +23,6 @@ class Shop {
         
 
       
-
-        // else {
-        //   if (this.items[i].quality < 50) {
-        //     this.items[i].quality = this.items[i].quality + 1;
-        //     if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-        //       if (this.items[i].sellIn < 11) {
-        //         if (this.items[i].quality < 50) {
-        //           this.items[i].quality = this.items[i].quality + 1;
-        //         }
-        //       }
-        //       if (this.items[i].sellIn < 6) {
-        //         if (this.items[i].quality < 50) {
-        //           this.items[i].quality = this.items[i].quality + 1;
-        //         }
-        //       }
-        //     }
-        //   }
-        // }
 
         else {
           if (this.items[i].quality < 50) {
