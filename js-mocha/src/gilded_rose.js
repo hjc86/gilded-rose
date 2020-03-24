d8c320c4879cb17f4c5af00489aa5b37c0355f45
Helder Carmen <heldercarmen@gmail.com>
2020-03-24 12:02:05 +0000

    improve with else if statements

--- js-mocha/src/gilded_rose.js
+++ js-mocha/src/gilded_rose.js
@@ -22,8 +22,6 @@ class Shop {
           }
         
 
-      
-
         else {
           if (this.items[i].quality < 50) {
           this.items[i].quality = this.items[i].quality + 1;
@@ -45,7 +43,10 @@ class Shop {
         this.items[i].sellIn = this.items[i].sellIn - 1;
       }
 
+
+      //=====================================
       if (this.items[i].sellIn < 0) {
+
         if (this.items[i].name != 'Aged Brie') {
           if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
             if (this.items[i].quality > 0) {
@@ -53,14 +54,49 @@ class Shop {
                 this.items[i].quality = this.items[i].quality - 1;
               }
             }
-          } else {
+          } 
+        
+        
+          else {
             this.items[i].quality = this.items[i].quality - this.items[i].quality;
           }
-        } else {
+        
+        
+        } 
+        
+        //else
+        else {
           if (this.items[i].quality < 50) {
             this.items[i].quality = this.items[i].quality + 1;
           }
         }
+      
+      //end
+      
+      }
+
+      //==============================================
+
+      if (this.items[i].sellIn < 0) {
+        x=true;
+
+        if (this.items[i].name != 'Aged Brie' &&this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert' && this.items[i].quality > 0 && this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+            this.items[i].quality = this.items[i].quality - 1;
+        }
+
+        else if(this.items[i].name != 'Aged Brie') {
+            this.items[i].quality = this.items[i].quality - this.items[i].quality;
+          }
+        
+        else {
+          if (this.items[i].quality < 50) {
+            this.items[i].quality = this.items[i].quality + 1;
+          }
+        }
+      
+      
+      
+      
       }
     
     
